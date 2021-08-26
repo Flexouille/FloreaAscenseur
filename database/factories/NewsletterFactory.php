e0772c34f0c30f31082e4af6f60e6dab7fea6348
Flexouille <quentin.leclercbte@gmail.com>
2021-08-26 18:15:07 +0200

    Initial commit

---
+++ database/factories/NewsletterFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Newsletter;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsletterFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Newsletter::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
